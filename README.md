90d80cde2037416d61c233afb5a43b7946651f7b
Kasper Primdal Lauritzen <git@kplauritzen.dk>
2022-09-23 18:40:15 +0200

    Day 1

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of Code 2021
+This is my attempt to solve Advent of Code 2021 in Rust
+
+https://adventofcode.com/2021
+
+## Quick start
+
+I don't know how to run anything yet
---
+++ src/bin/day01.rs
@@ -0,0 +1,84 @@
+// https://adventofcode.com/2021/day/1
+
+use aoc_rust_2021::{file_to_vec, string_to_int};
+fn main() {
+    let filename = "input/day01.txt".to_string();
+    let input = file_to_vec(filename).unwrap();
+    let part_1_result = part_1(input.clone());
+    println!("Part 1: {}", part_1_result);
+
+    let part_2_result = part_2(input.clone());
+    println!("Part 2: {}", part_2_result);
+}
+
+fn part_1(input: Vec<String>) -> i32 {
+    let depths = string_to_int(input);
+    // iterate over neighboring values and count increases
+    let mut count = 0;
+    for i in 0..depths.len() {
+        // First measurement does not have a previous value
+        if i == 0 {
+            continue;
+        }
+        if depths[i] > depths[i - 1] {
+            count += 1;
+        }
+    }
+    count
+}
+
+fn part_2(input: Vec<String>) -> i32 {
+    let depths = string_to_int(input);
+    // iterate over 3 away neighbor values and count increases
+    let mut count = 0;
+    for i in 0..depths.len() {
+        // First measurement does not have a previous value
+        if i < 3 {
+            continue;
+        }
+        if depths[i] > depths[i - 3] {
+            count += 1;
+        }
+    }
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use aoc_rust_2021::file_to_vec;
+    // Part 1
+    #[test]
+    fn part_1_sample_input() {
+        use crate::part_1;
+        let filename = "input/day01_sample.txt".to_string();
+        let input = file_to_vec(filename).unwrap();
+        let result = part_1(input);
+        assert_eq!(result, 7);
+    }
+    #[test]
+    fn part_1_full_input() {
+        use crate::part_1;
+        let filename = "input/day01.txt".to_string();
+        let input = file_to_vec(filename).unwrap();
+        let result = part_1(input);
+        assert_eq!(result, 1766);
+    }
+
+    // Part 2
+    #[test]
+    fn part_2_sample_input() {
+        use crate::part_2;
+        let filename = "input/day01_sample.txt".to_string();
+        let input = file_to_vec(filename).unwrap();
+        let result = part_2(input);
+        assert_eq!(result, 5);
+    }
+    #[test]
+    fn part_2_full_input() {
+        use crate::part_2;
+        let filename = "input/day01.txt".to_string();
+        let input = file_to_vec(filename).unwrap();
+        let result = part_2(input);
+        assert_eq!(result, 1797);
+    }
+}
